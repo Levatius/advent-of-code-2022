13a2934d047cc99b49c174626226820246a8123b
Lev <levatius@hotmail.co.uk>
2022-12-06 13:34:24 +0000

    Latest changes

---
+++ day_00/main.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2022
+
+from dataclasses import dataclass
+
+
+def get_data(file_name='input.txt'):
+    with open(file_name) as f:
+        pass
+
+
+def run():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
