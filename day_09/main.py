33eaf295bfe4890784acadfc2aab2ed8924fb43a
Lev <levatius@hotmail.co.uk>
2022-12-09 14:36:34 +0000

    Latest changes

--- day_09/main.py
+++ day_09/main.py
@@ -4,35 +4,25 @@ import numpy as np
 from dataclasses import dataclass, field
 from more_itertools import windowed
 
-DIRECTIONS_MAP = {
-    'L': np.array([-1, 0]),
-    'R': np.array([1, 0]),
-    'U': np.array([0, 1]),
-    'D': np.array([0, -1]),
-    'LU': np.array([-1, 1]),
-    'RU': np.array([1, 1]),
-    'LD': np.array([-1, -1]),
-    'RD': np.array([1, -1])
-}
-
 
 @dataclass
 class Movement:
-    direction: str
+    DIRECTIONS_MAP = {'L': np.array([-1, 0]), 'R': np.array([1, 0]), 'U': np.array([0, 1]), 'D': np.array([0, -1])}
+    direction: np.array
     steps: int
 
     @classmethod
     def from_movement_str(cls, movement_str):
-        direction, steps = movement_str.split(' ')
-        return cls(direction, int(steps))
+        direction_str, steps_str = movement_str.split(' ')
+        return cls(cls.DIRECTIONS_MAP[direction_str], int(steps_str))
 
 
 @dataclass
 class Knot:
-    pos: np.ndarray = field(default_factory=lambda: np.zeros(2))
+    pos: np.array = field(default_factory=lambda: np.zeros(2))
 
     def move(self, direction):
-        self.pos += DIRECTIONS_MAP[direction]
+        self.pos += direction
 
 
 class Rope:
@@ -57,18 +47,11 @@ class Rope:
         # Two knots are considered stable if they are adjacent or diagonal to each other
         return self.distance(knot_a, knot_b) <= 2 ** 0.5
 
-    def log_tail_pos(self):
-        self.tail_pos_log.add(tuple(self.tail.pos))
-
-    def get_stabilising_direction(self, leading_knot, tailing_knot):
-        # Look at all 8 positions around the tailing knot and find which one is the most stable with the leading knot
-        # Likely could be optimised by only looking at positions "towards" the leading knot
-        direction_distances = {}
-        for direction, vector in DIRECTIONS_MAP.items():
-            new_tailing_knot = Knot(pos=tailing_knot.pos + vector)
-            distance = self.distance(leading_knot, new_tailing_knot)
-            direction_distances[direction] = distance
-        return min(direction_distances, key=direction_distances.get)
+    @staticmethod
+    def get_stabilising_direction(leading_knot, tailing_knot):
+        # Example: If leading knot is at (2,1) and tailing knot is at (0,0), stabilising direction = (1,1)
+        direction = np.fromiter(map(np.sign, leading_knot.pos - tailing_knot.pos), dtype=int)
+        return direction
 
     def move(self, direction):
         self.head.move(direction)
@@ -79,6 +62,9 @@ class Rope:
             stabilising_direction = self.get_stabilising_direction(leading_knot, tailing_knot)
             tailing_knot.move(stabilising_direction)
         self.log_tail_pos()
+
+    def log_tail_pos(self):
+        self.tail_pos_log.add(tuple(self.tail.pos))
 
 
 def get_data(file_name='input.txt'):
